a1012256f8f64c5423f121041db9b8db427f2455
Tyler Cox <tylercox@Tylers-MacBook-Pro.local>
2017-08-12 17:13:05 -0500

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name megabyteOrganicsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the megabyteOrganicsApp
+ */
+angular.module('megabyteOrganicsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
