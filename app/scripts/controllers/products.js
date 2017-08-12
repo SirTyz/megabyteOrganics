c3bb4eea7e6f49692868b95536b509b97a34d71f
tcox427 <tcox427@gmail.com>
2017-08-12 18:31:55 -0500

    added new route

---
+++ app/scripts/controllers/products.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name megabyteOrganicsApp.controller:ProductsCtrl
+ * @description
+ * # ProductsCtrl
+ * Controller of the megabyteOrganicsApp
+ */
+angular.module('megabyteOrganicsApp')
+  .controller('ProductsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
